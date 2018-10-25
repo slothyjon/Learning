02e8e9006ce576a0793d60f3d8b831028e3d675c
slothyjon <37322752+slothyjon@users.noreply.github.com>
2018-10-24 22:25:59 -0700

    uploading project

---
+++ Assets/Scripts/PickUpObjectGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObjectGenerator : MonoBehaviour {
+
+    public GameObject PickUpPrefabReference = null;
+
+    private const float SPAWN_RANGE = 9.5f;
+    private const float HEIGHT = 1f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GeneratePickUpObjects(12);		
+	}
+
+    private void GeneratePickUpObjects(int num)
+    {
+        for (int i=0; i < num; i++)
+        {
+            GameObject temp = Instantiate(PickUpPrefabReference, transform);
+            temp.transform.localPosition = new Vector3(Random.Range(-SPAWN_RANGE, SPAWN_RANGE), HEIGHT, Random.Range(-SPAWN_RANGE, SPAWN_RANGE));
+        }
+    }
+}
---
+++ Assets/TableController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableController : MonoBehaviour {
+
+    private const float MOVEMENT_SPEED = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.right, moveVertical * MOVEMENT_SPEED);
+        transform.Rotate(Vector3.back, moveHorizontal * MOVEMENT_SPEED);
+    }
+}
